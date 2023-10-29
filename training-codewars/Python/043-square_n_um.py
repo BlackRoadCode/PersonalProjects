ecca2af810db18eab12a49c2b5acb90f56513c56
BlackRoadCode <blackroadcode@gmail.com>
2023-10-29 15:50:55 -0600

    added katas

---
+++ training-codewars/Python/043-square_n_um.py
@@ -0,0 +1,18 @@
+"""
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9 
+"""
+
+def square_sum(numbers):
+    counter = 0
+    for i in range( len(numbers) ):
+        counter += numbers[i]**2
+    return counter
+
+print( square_sum([1,2,2]) )
+
+"""
+def square_sum(numbers):
+    return sum(x ** 2 for x in numbers)
+"""
---
+++ training-codewars/Python/044-reversed_strings.py
@@ -0,0 +1,12 @@
+"""
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+"""
+
+def solution(string):
+    return string[::-1]
+
+print( solution('pepex') )
+print( solution('sosay') )
